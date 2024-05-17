798471f8fe50a521420e8f480093f643f4855364
mohammedALhakmy <mohammed.devlopement.yemen@gmail.com>
2024-05-17 17:01:35 +0300

    serveice_one edit

--- service_one.php
+++ service_one.php
@@ -1,31 +1,42 @@
 <?php
 include "db_conn.php";
 include 'master.php';
+
+
 if (isset($_GET['id']) && is_numeric($_GET['id'])){
     $id = $_GET['id'];
     $stmt = $conn->prepare("SELECT * FROM `sevices` WHERE id = :id");
     $stmt->bindParam(':id', $id);
     $stmt->execute();
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
-    ?>
-    <div class="col-md-8">
-
-    </div>
-    <div class="col-md-4">
-        <div class="card1-group">
-            <div class="card1">
-                <div class="titlee"><p><?php echo $result['service_name']; ?></p></div>
-                <img src="img/<?php echo $result['service_img']; ?>">
-                <div class="layer"></div>
-                <div class="info">
-                    <p><?php echo $result['service_notes']; ?></p>
-                    <button> للمزيد</button>
+    echo '<div class="row">
+            <div class="col-md-8"></div>';
+    if ($stmt->rowCount() > 0 ){
+        echo '
+        <div class="col-md-4">
+            <div class="card1-group">
+                <div class="card1">
+                    <div class="titlee"><p>'.$result['service_name'].'</p></div>
+                    <img src="img/'.$result['service_img'].'">
+                    <div class="layer"></div>
+                    <div class="info">
+                        <p>'.$result['service_notes'].'</p>
+                        <button> للمزيد</button>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
+        </div>';
+    } else{
 
-
-<?php
-    include 'include/footer.php';
+        echo  ' <div class="col-md-4 text-danger"> 
+           <div class="card1-group">
+ Not Found Any Content</div>
+ </div>';
+    }
+    echo '</div>';
+} else {
+    echo  "Not Found Any Content";
 }
+
+include 'include/footer.php';
+?>
